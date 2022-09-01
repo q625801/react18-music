ee67b0e406f1f859f69ed10da598841cdef78c90
王誉 <wangyu@yunjiacloud.com>
2022-09-01 18:28:53 +0800

    1.新增redux相关功能

---
+++ src/components/audio/store/reducer.js
@@ -0,0 +1,55 @@
+const initState = {
+    //播放器
+    audioInfo:{
+        audioFlag:false,//是否显示音频播放器
+        audioPlayBtn:false,//播放器开始按钮状态
+        SongInfo:{//当前正在播放的音乐数据
+            SongId:'',
+            SongName:'',
+            SongPic:'',
+            SongArtists:''
+        },
+        SongListFlag:false,
+        SongList:[],//播放列表数据
+    },
+    //播放器end
+}
+const reducer = (state = initState,action) => {
+    // console.log("reducer：",state,action)
+    switch (action.type) {
+        case 'setAudioFlag':
+            state.audioInfo.audioFlag = action.value
+            return state
+        case 'setAudioPlayBtn':
+            state.audioInfo.audioPlayBtn = action.value
+            return state
+        case 'setAudioInfo':
+            if(state.audioInfo.SongInfo.SongId === action.SongInfo.SongId){
+                return state
+            }
+            state.audioInfo.SongInfo = action.SongInfo
+            state.audioInfo.SongList = action.SongList
+            return state
+        case 'setSongInfo':
+            if(state.audioInfo.SongInfo.SongId === action.value.SongId){
+                return state
+            }
+            return {
+                audioInfo:{
+                    ...state.audioInfo,
+                    SongInfo:{...action.value}
+                }
+            }
+        case 'setSongList':
+            state.audioInfo.SongList = action.value
+            return state
+        case 'setSongInfoInit':
+            state.audioInfo.SongInfo = action.value
+            return state
+        default:
+            return state
+    }
+}
+module.exports = {
+    reducer
+}
